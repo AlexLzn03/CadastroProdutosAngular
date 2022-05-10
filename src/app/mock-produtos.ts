3fd579431e3bf6be4bb2acf2cfb1bb0b6bb2f52d
AlexLzn03 <alexsanderlozano175474@gmail.com>
2022-05-10 11:07:41 -0300

    teste

--- src/app/mock-produtos.ts
+++ src/app/mock-produtos.ts
@@ -6,7 +6,7 @@ export const PRODUTOS: Produto[] = [
   { id: 3, name: 'Playstation4 500gb' },
   { id: 4, name: 'Motorola G7 Power' },
   { id: 5, name: 'Playstation3 250gb' },
-  { id: 6, name: 'Ryzen5 3500' },
+  { id: 6, name: 'Ryzen5 500' },
   { id: 7, name: 'GTX 3070Ti' },
   { id: 8, name: 'GTX 750Ti' },
   { id: 9, name: 'TV Samsung 32' },
