b471b2373a72e20091de92e61f357b6880b4292e
AlexLzn03 <alexsanderlozano175474@gmail.com>
2022-05-10 10:59:49 -0300

    final

---
+++ src/app/mock-produtos.ts
@@ -0,0 +1,14 @@
+import { Produto } from './produto';
+
+export const PRODUTOS: Produto[] = [
+  { id: 1, name: 'TV LG 53' },
+  { id: 2, name: 'Acer Aspire 3' },
+  { id: 3, name: 'Playstation4 500gb' },
+  { id: 4, name: 'Motorola G7 Power' },
+  { id: 5, name: 'Playstation3 250gb' },
+  { id: 6, name: 'Ryzen5 3500' },
+  { id: 7, name: 'GTX 3070Ti' },
+  { id: 8, name: 'GTX 750Ti' },
+  { id: 9, name: 'TV Samsung 32' },
+  { id: 10, name: 'Iphone 12' }
+];
